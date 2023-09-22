851c86fcb6595b2d4a53d5a6437fd520183bf3fb
qeaml <qeaml@proton.me>
2023-09-22 16:48:24 +0200

    🚧 codegen: First implementation

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,32 @@
 const std = @import("std");
-const lex = @import("lex.zig");
+const parse = @import("parse.zig");
+const codegen = @import("codegen.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    const tokens = try lex.parseAll(allocator, "test", "\"pee poo\" >= ()");
-    defer allocator.free(tokens);
+    const args = try std.process.argsAlloc(allocator);
+    defer allocator.free(args);
+
+    if (args.len < 2) {
+        std.debug.print("Provide a filename", .{});
+        return;
+    }
+
+    const filename = args[1];
+    const file = try std.fs.cwd().openFile(filename, .{});
+    defer file.close();
+    const source = try file.reader().readAllAlloc(allocator, 102400);
+    defer allocator.free(source);
+
+    const format = try parse.all(allocator, filename, source);
+    const header = std.ArrayList(u8).init(allocator);
+    defer header.deinit();
+    const reader = std.ArrayList(u8).init(allocator);
+    defer reader.deinit();
+    const writer = std.ArrayList(u8).init(allocator);
+    defer writer.deinit();
+
+    try codegen.write(format, std.io.getStdOut().writer(), std.io.getStdOut().writer(), std.io.getStdOut().writer());
 }
